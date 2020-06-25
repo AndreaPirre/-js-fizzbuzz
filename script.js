be6285f942e1fd6d96192296dccdfb2200b1d506
Andrea <andreapirrello1990@gmail.com>
2020-06-25 15:33:08 +0200

    sostituiti i multipli

--- script.js
+++ script.js
@@ -6,7 +6,14 @@
 // Per i multipli di 5 stampi "Buzz" al posto del numero.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero.
 
-
-for (var i=1; i<=100; i++) {
-      console.log(i);
-    }
+for (var i=1; i <= 100; i++)
+{
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
+}
